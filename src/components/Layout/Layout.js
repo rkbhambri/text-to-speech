41d2133e06e053ab5a7140eb5bcadf36043481c6
ram <ramkrishan@kontikilabs.com>
2019-05-15 12:57:58 +0530

    Minor change in ui

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -6,7 +6,7 @@ import Controls from '../Controls/Controls';
 const Layout = (props) => {
     return (
         <div className="layout col-md-10 offset-1 mt-5 text-center">
-            <div className="message-controls col-md-5 offset-3">
+            <div className="message-controls col-md-6 offset-3">
                 <Heading heading="Text To Speech" />
                 <Controls />
             </div>
