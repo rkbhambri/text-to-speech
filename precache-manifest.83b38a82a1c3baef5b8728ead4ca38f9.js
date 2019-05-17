a65ccd32003e6ec1c4125dea0931be0fc033fcfe
ram <ramkrishan@kontikilabs.com>
2019-05-17 22:45:01 +0530

    Updates

---
+++ precache-manifest.83b38a82a1c3baef5b8728ead4ca38f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f84493f75860f994ecbb56ef6a86ff21",
+    "url": "/text-to-speech/index.html"
+  },
+  {
+    "revision": "a4729c798752f583cc76",
+    "url": "/text-to-speech/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "fa0a616a1481e7dda563",
+    "url": "/text-to-speech/static/css/main.1779617f.chunk.css"
+  },
+  {
+    "revision": "a4729c798752f583cc76",
+    "url": "/text-to-speech/static/js/2.26513579.chunk.js"
+  },
+  {
+    "revision": "fa0a616a1481e7dda563",
+    "url": "/text-to-speech/static/js/main.043b5b13.chunk.js"
+  },
+  {
+    "revision": "b9d426ee69f92f7eb0a1",
+    "url": "/text-to-speech/static/js/runtime~main.c832fb67.js"
+  }
+]);
